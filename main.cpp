39413580b99d439b149f3cdd9ca92c3b018b44b9
kzoli39 <71815260+kzoli39@users.noreply.github.com>
2022-09-13 15:32:06 +0200

    javitott main.cpp

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];            // N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa'  // ; hiányzik
-    for (int i = 0;)                        // nincs megadva, hogy meddig iteráljon és milyen lépésközzel
+    int *b = new int[N_ELEMENTS];            // N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa';  // ; hiányzik
+    for (int i = 0; i < N_ELEMENTS; i++)     // nincs megadva, hogy meddig iteráljon és milyen lépésközzel
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)                 //csak egyszer fut le a ciklus
+    for (int i = 0; i < N_ELEMENTS; i++)     //csak egyszer fut le a ciklus
     {
-        std::cout << "Ertek:"               // ; hiányzik, nincs érték kiíratás
+        std::cout << "Ertek:" << b[i];          // ; hiányzik, nincs érték kiíratás
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;                              // int helyett double típust kellene használni
-    for (int i = 0; i < N_ELEMENTS, i++)    // , helyett ; kell
+    for (int i = 0; i < N_ELEMENTS; i++)    // , helyett ; kell
     {
-        atlag += b[i]                       // ; hiányzik
+        atlag += b[i];                      // ; hiányzik
     }
-    atlag /= N_ELEMENTS;                    //csak egy sima osztás kéne
+    atlag = atlag / N_ELEMENTS;                    //csak egy sima osztás kéne
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
